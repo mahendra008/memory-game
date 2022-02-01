7225bdc2293ef1db7d0f8400191bca2f149fe428
Mahendra Kshirsagar <mahendra008@gmail.com>
2022-02-01 13:31:10 +0530

    removed nested files

--- src/components/App.js
+++ src/components/App.js
@@ -69,7 +69,7 @@ function App() {
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button onClick={shuffleCards}>New Game</button>\
+      <button onClick={shuffleCards}>New Game</button>
         <div className='card-grid'>
           {cards.map(card => (
               <SingleCard 
